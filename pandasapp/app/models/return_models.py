d65cd4ca8a9d63069655bfbe7347740ed2999a92
Bjørn Göttler <bjoern@goettler.dk>
2024-12-17 20:41:02 +0100

    feature: Compute service: Add minimalistic app

--- pandasapp/app/models/return_models.py
+++ pandasapp/app/models/return_models.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from pydantic import BaseModel
 
 class StatisticsReturnModel(BaseModel):
-    computed: datetime
     amount_max: float
     amount_min: float
     amount_avg: float
