ad2d9fb7fafecfed991869de912d9dcb6fca3ba3
Bjørn Göttler <bjoern@goettler.dk>
2024-12-17 20:40:35 +0100

    feature: Compute service: Add the very basics

---
+++ pandasapp/app/models/return_models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+class StatisticsReturnModel(BaseModel):
+    computed: datetime
+    amount_max: float
+    amount_min: float
+    amount_avg: float
+    amount_sum: float
+    amount_count: int
+    amount_std: float
+    amount_median: float
+
