62c0bfc2ceadd0b2ca61d1b6a2f57d018f4d7d7d
Bjørn Göttler <bjoern@goettler.dk>
2024-12-18 19:19:25 +0100

    feature: DataService: Add Json Naming Convention

--- DataService/JsonNamingpolicies/SnakeCaseNamingPolicy.cs
+++ DataService/JsonNamingpolicies/SnakeCaseNamingPolicy.cs
@@ -1,6 +1,17 @@
+using System.Text.Json;
+
 namespace DataService.JsonNamingpolicies;
 
-public class SnakeCaseNamingPolicy
+public class SnakeCaseNamingPolicy: JsonNamingPolicy
 {
+    public override string ConvertName(string name)
+    {
+        var words = name.Split("_");
+        for (var i = 0; i < words.Length; i++)
+        {
+            words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1);
+        }
+        return string.Concat(words);
+    }
     
 }
