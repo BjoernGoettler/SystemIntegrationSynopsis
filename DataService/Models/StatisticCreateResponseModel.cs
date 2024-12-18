7454eb87bcded5cb0207311423065b5ef936eb9a
Bjørn Göttler <bjoern@goettler.dk>
2024-12-18 19:16:45 +0100

    feature: DataService: Update models

--- DataService/Models/StatisticCreateResponseModel.cs
+++ DataService/Models/StatisticCreateResponseModel.cs
@@ -1,12 +1,21 @@
+using System.Text.Json.Serialization;
+
 namespace DataService.Models;
 
-public class StatisticCreateResponse
+public class StatisticCreateResponseModel
 {
+    [JsonPropertyName("amount_max")]
     public float AmountMax { get; set; }
+    [JsonPropertyName("amount_min")]
     public float AmountMin { get; set; }
+    [JsonPropertyName("amount_avg")]
     public float AmountAverage { get; set; }
+    [JsonPropertyName("amount_sum")]
     public float AmountSum { get; set; }
+    [JsonPropertyName("amount_count")]
     public int AmountCount { get; set; }
+    [JsonPropertyName("amount_std")]
     public float AmountStdDev { get; set; }
+    [JsonPropertyName("amount_median")]
     public float AmountMedian { get; set; }
 }
